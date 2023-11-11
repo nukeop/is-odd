372b0f575b575026f23cb8ae89f0d7833d34c207
nukeop <12746779+nukeop@users.noreply.github.com>
2023-11-11 22:03:33 +0100

    Tests for usize and isize

--- tests/tests.rs
+++ tests/tests.rs
@@ -6,189 +6,217 @@ mod tests {
 
     #[test]
     fn number_i8_is_odd() {
-
-        let mut _i : i8 = -1;
+        let mut _i: i8 = -1;
         assert!(_i.is_odd());
 
-        _i=0;
+        _i = 0;
         assert!(!_i.is_odd());
 
-        _i=1;
+        _i = 1;
         assert!(_i.is_odd());
 
-        _i=2;
+        _i = 2;
         assert!(!_i.is_odd());
     }
 
     #[test]
     fn number_u8_is_odd() {
-
-        let mut _i : u8 = 0;
+        let mut _i: u8 = 0;
         assert!(!_i.is_odd());
 
-        _i=1;
+        _i = 1;
         assert!(_i.is_odd());
 
-        _i=2;
+        _i = 2;
         assert!(!_i.is_odd());
 
-        _i=3;
+        _i = 3;
         assert!(_i.is_odd());
     }
 
     #[test]
     fn number_i16_is_odd() {
-
-        let mut _i : i16 = -1;
+        let mut _i: i16 = -1;
         assert!(_i.is_odd());
 
-        _i=0;
+        _i = 0;
         assert!(!_i.is_odd());
 
-        _i=1;
+        _i = 1;
         assert!(_i.is_odd());
 
-        _i=2;
+        _i = 2;
         assert!(!_i.is_odd());
     }
 
     #[test]
     fn number_u16_is_odd() {
-
-        let mut _i : u16 = 0;
+        let mut _i: u16 = 0;
         assert!(!_i.is_odd());
 
-        _i=1;
+        _i = 1;
         assert!(_i.is_odd());
 
-        _i=2;
+        _i = 2;
         assert!(!_i.is_odd());
 
-        _i=3;
+        _i = 3;
         assert!(_i.is_odd());
     }
 
-        #[test]
+    #[test]
     fn number_i32_is_odd() {
-
-        let mut _i : i32 = -2;
+        let mut _i: i32 = -2;
         assert!(!_i.is_odd());
 
-        _i=-1;
+        _i = -1;
         assert!(_i.is_odd());
 
-        _i=0;
-        assert!(!_i.is_odd());
-        
-        _i=1;
-        assert!(_i.is_odd());
-
-        _i=2;
+        _i = 0;
         assert!(!_i.is_odd());
 
-        _i=3;
+        _i = 1;
         assert!(_i.is_odd());
 
-        _i=10_000_000;
+        _i = 2;
+        assert!(!_i.is_odd());
+
+        _i = 3;
+        assert!(_i.is_odd());
+
+        _i = 10_000_000;
         assert!(!_i.is_odd());
     }
 
     #[test]
     fn number_u32_is_odd() {
-
-        let mut _i : u32 = 0;
+        let mut _i: u32 = 0;
         assert!(!_i.is_odd());
 
-        _i=1;
+        _i = 1;
         assert!(_i.is_odd());
 
-        _i=2;
+        _i = 2;
         assert!(!_i.is_odd());
 
-        _i=3;
+        _i = 3;
         assert!(_i.is_odd());
     }
 
     #[test]
     fn number_i64_is_odd() {
-
-        let mut _i : i64 = -2;
+        let mut _i: i64 = -2;
         assert!(!_i.is_odd());
 
-        _i=-1;
+        _i = -1;
         assert!(_i.is_odd());
 
-        _i=0;
-        assert!(!_i.is_odd());
-        
-        _i=1;
-        assert!(_i.is_odd());
-
-        _i=2;
+        _i = 0;
         assert!(!_i.is_odd());
 
-        _i=3;
+        _i = 1;
         assert!(_i.is_odd());
 
-        _i=10_000_000;
+        _i = 2;
+        assert!(!_i.is_odd());
+
+        _i = 3;
+        assert!(_i.is_odd());
+
+        _i = 10_000_000;
         assert!(!_i.is_odd());
     }
 
     #[test]
     fn number_u64_is_odd() {
-
-        let mut _i : u64 = 0;
+        let mut _i: u64 = 0;
         assert!(!_i.is_odd());
 
-        _i=1;
+        _i = 1;
         assert!(_i.is_odd());
 
-        _i=2;
+        _i = 2;
         assert!(!_i.is_odd());
 
-        _i=3;
+        _i = 3;
         assert!(_i.is_odd());
     }
-    
+
     #[test]
     fn number_i128_is_odd() {
-
-        let mut _i : i128 = -2;
+        let mut _i: i128 = -2;
         assert!(!_i.is_odd());
 
-        _i=-1;
+        _i = -1;
         assert!(_i.is_odd());
 
-        _i=0;
-        assert!(!_i.is_odd());
-        
-        _i=1;
-        assert!(_i.is_odd());
-
-        _i=2;
+        _i = 0;
         assert!(!_i.is_odd());
 
-        _i=3;
+        _i = 1;
         assert!(_i.is_odd());
 
-        _i=10_000_000;
+        _i = 2;
+        assert!(!_i.is_odd());
+
+        _i = 3;
+        assert!(_i.is_odd());
+
+        _i = 10_000_000;
         assert!(!_i.is_odd());
     }
-    
+
     #[test]
     fn number_u128_is_odd() {
-
-        let mut _i : u128 = 0;
+        let mut _i: u128 = 0;
         assert!(!_i.is_odd());
 
-        _i=1;
+        _i = 1;
         assert!(_i.is_odd());
 
-        _i=2;
+        _i = 2;
         assert!(!_i.is_odd());
 
-        _i=3;
+        _i = 3;
         assert!(_i.is_odd());
     }
-    
+
+    #[test]
+    fn number_usize_is_odd() {
+        let mut _i: usize = 0;
+        assert!(!_i.is_odd());
+
+        _i = 1;
+        assert!(_i.is_odd());
+
+        _i = 2;
+        assert!(!_i.is_odd());
+
+        _i = 3;
+        assert!(_i.is_odd());
+    }
+
+    #[test]
+    fn number_isize_is_odd() {
+        let mut _i: isize = -2;
+        assert!(!_i.is_odd());
+
+        _i = -1;
+        assert!(_i.is_odd());
+
+        _i = 0;
+        assert!(!_i.is_odd());
+
+        _i = 1;
+        assert!(_i.is_odd());
+
+        _i = 2;
+        assert!(!_i.is_odd());
+
+        _i = 3;
+        assert!(_i.is_odd());
+
+        _i = 10_000_000;
+        assert!(!_i.is_odd());
+    }
 }
