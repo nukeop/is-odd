0840002949fb2dfb4895a75ca1bad970a77f0289
mateolafalce <mateolafalce3@gmail.com>
2024-12-23 14:37:42 -0300

    1.1.1 proposal + floating support

--- src/lib.rs
+++ src/lib.rs
@@ -15,3 +15,23 @@ macro_rules! prim_impl {
 }
 
 prim_impl!(i8 u8 i16 u16 i32 u32 i64 u64 i128 u128 isize usize);
+
+impl IsOdd for f32 {
+    fn is_odd(&self) -> bool {
+        if self.is_finite() && self.fract() == 0.0 {
+            (*self as i32).is_odd()
+        } else {
+            false
+        }
+    }
+}
+
+impl IsOdd for f64 {
+    fn is_odd(&self) -> bool {
+        if self.is_finite() && self.fract() == 0.0 {
+            (*self as i64).is_odd()
+        } else {
+            false
+        }
+    }
+}
